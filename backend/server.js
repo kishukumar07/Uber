f273dcef12aacffab9e80cb5eb8bb984306215b6
kishukumar07 <kishukumars082@gmail.com>
2025-01-28 16:30:38 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+
+const http =require('http');
+const app =require('./app');
+const port =process.env.PORT || 3000; 
+
+
+const server =http.createServer(app)
+
+
+
+
+
+app.listen(port,()=>{
+    console.log(`Server is live at ${port}`);
+    })
+    
